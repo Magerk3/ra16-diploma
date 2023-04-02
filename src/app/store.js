2057863cbdf15526e29b70fe85a338ea746ea1a6
Nikolai <magerk38@gmail.com>
2023-04-02 18:15:28 +0800

    final?

--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import  bestsellersReducer from '../features/bestsellers/bestsellersSlice'
-import catalogReducer from '../features/catalog/catalogSlice';
-import searchReducer from '../features/search/searchBarSlice'
-import productPageReducer from '../features/productPage/productPageSlice';
-import cartReducer from '../features/cart/cartSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import bestsellersReducer from "../features/bestsellers/bestsellersSlice";
+import catalogReducer from "../features/catalog/catalogSlice";
+import searchReducer from "../features/search/searchBarSlice";
+import productPageReducer from "../features/productPage/productPageSlice";
+import cartReducer from "../features/cart/cartSlice";
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    bestsellers: bestsellersReducer,
-    catalog: catalogReducer,
-    search: searchReducer,
-    productPage: productPageReducer,
-    cart: cartReducer,
-  },
+    reducer: {
+        bestsellers: bestsellersReducer,
+        catalog: catalogReducer,
+        search: searchReducer,
+        productPage: productPageReducer,
+        cart: cartReducer,
+    },
 });
